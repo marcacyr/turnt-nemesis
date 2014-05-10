6393b8fc4c960a609886b0958b45a4eba42f59e2
Marc Cyr <marcacyr@gmail.com>
2014-05-10 14:52:40 -0700

    cleanup files in tree. add teams model view controller.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130611223312) do
+ActiveRecord::Schema.define(:version => 20140510213508) do
 
   create_table "customers", :force => true do |t|
     t.string   "company"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(:version => 20130611223312) do
   end
 
   add_index "customers", ["user_id"], :name => "index_customers_on_user_id"
+
+  create_table "teams", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
